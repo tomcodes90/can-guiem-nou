691f885ce21040f4b25a05dd3e77d02d18c64870
Thomas Miccichè <97365235+tomcodes90@users.noreply.github.com>
2026-02-18 13:44:55 +0100

    sanity.ts fix

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,6 +1,6 @@
-import { createClient } from 'next-sanity'
+import {createClient} from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
-import {SanityImageObject, SanityImageWithAssetStub} from "@sanity/image-url/lib/types/types";
+import type {SanityImageSource} from '@sanity/image-url/lib/types/types'
 
 export const client = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
@@ -11,6 +11,6 @@ export const client = createClient({
 
 const builder = imageUrlBuilder(client)
 
-export function urlFor(source: string | SanityReference | SanityAsset | SanityImageObject | SanityImageWithAssetStub) {
+export function urlFor(source: SanityImageSource) {
     return builder.image(source)
 }
