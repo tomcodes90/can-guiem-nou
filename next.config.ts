dc3dae6bb4df18bd6de5032cb9b038c5d6beeffc
Thomas Miccichè <97365235+tomcodes90@users.noreply.github.com>
2026-02-18 13:41:33 +0100

    next.config.ts fix

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin('./request.ts');
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'cdn.sanity.io',
-                pathname: '/**',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
